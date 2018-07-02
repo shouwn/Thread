befe762cffee701bae1d3257840f6e66cb92d6a6
shouwn <sjonghyon@gmail.com>
2018-07-02 22:08:52 +0900

    상속과 구현으로 수정

--- src/basic/ThreadMake.java
+++ src/basic/ThreadMake.java
@@ -1,34 +1,22 @@
 package basic;
 
-public class ThreadMake implements Runnable{
+public class ThreadMake extends Thread implements Runnable{
 
 	public static void main(String[] args) {
 		Thread t1 =
 				new Thread(new ThreadMake());
-		
+
 		Thread t2 =
-				new Thread(new ThreadMake());
+				new ThreadMake();
 
 		t1.start();
 		t2.start();
-		
-		print();
 	}
 
 	@Override
 	public void run() {
 
-		int i = 0;
-		while(i++ < 10) {
-			ThreadMake.print();
-			try {
-				System.exit(0);
-				Thread.sleep(100);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
+		ThreadMake.print();
 	}
 
 	public static void print() {
